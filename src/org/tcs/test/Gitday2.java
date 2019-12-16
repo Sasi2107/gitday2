b62365af0bdd1ebdd6e537762dfbb4260019e1f7
Sasi2107 <sasisaranya0407@gmail.com>
2019-12-16 20:26:46 +0530

    day 2 code added

---
+++ src/org/tcs/test/Gitday2.java
@@ -0,0 +1,21 @@
+package org.tcs.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Gitday2 {
+	static WebDriver driver;
+	public static void browserLaunch() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sasi\\eclipse-workspace\\Reusable\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	public static void launchUrl(String url) {
+		driver.get(url);				
+						 }
+	public static void dragNDrop(WebElement e, WebElement t) {
+		Actions ac = new Actions(driver);
+		ac.dragAndDrop(e, t).perform();
+}
+	}
